e37dcb5dce041f3afb0a6843bb86ccf23f9ab1ec
Kola Heyward-Rotimi <ttraveller7@gmail.com>
2020-11-27 15:31:08 -0500

    Bio Rewrite
    Rewrote the bio, fixed scrolling for the background image.

--- src/About.js
+++ src/About.js
@@ -13,11 +13,12 @@ import Theme from './Theme';
 
 const Background = styled.div`
         background-image: url(${backgroundAbout});
-        width: min-vw-100;
-        height: min-vh-100;
+        width: 100vw;
+        height: 100vh;
         background-size: cover;
-        position: center;
+        background-attachment: fixed;
         background-repeat: no-repeat;
+        overflow: auto;
 `;
 
 const Text = styled.div`
@@ -37,32 +38,25 @@ const ProfPic = styled.div`
 export const About = () => {
     return (
         <Background>
-            <div class = "container-fluid min-vh-100">
             <Theme>
-            <NaviBar></NaviBar>
-                <div class = "row-fluid">
-                    <div class = "col d-flex justify-content-center">
-                        <TextBlock>
-                            <ProfPic>
-                                <Image alt = "profile pic" src={headshot} fluid class = "img-fluid" width = "300" height = "300"></Image>
-                            </ProfPic>
-                        </TextBlock>
+                <div class = "container-fluid" style={{height: "100vh"}}>
+                    <div class = "row-fluid">
+                        <NaviBar></NaviBar>
                     </div>
-                    <div class = "col d-flex justify-content-center">
-                        <TextBlock>
-                            <Layout>
-                                <Text>
-                                    <h4>Kola Heyward-Rotimi is a Faculty Assistant at the University of Maryland's <a href="https://aadhum.umd.edu/">African-American History, Culture, and Digital Humanities initiative (AADHum)</a>. He's originally from Durham, NC and he spent four years of his adolescence in Ile-Ife, Nigeria--his father’s birthplace and one of the cultural touchstones that he incorporates into his scholarship and genre-blending creative writing.</h4>
-                                    <h4>In the digital humanities, Kola is focused on exploring the dynamics of emerging technologies in relation to marginalized communities within the US and abroad. His undergraduate thesis specifically looked at how these communities used virtual spaces and technology in ways that subverted their mainstream, intended use. These moments of disruption are his primary interest and Kola continues to expand on this work through his position at AADHum.</h4>
-                                    <h4>His fiction is also inspired by these cultural and technological intersections--the new ways that the material world changes how people engage with each other. Though these narratives tend to manifest through prose, Kola has also developed mixed media projects implementing video, glitch art, and fiction. Kola’s speculative short stories can be found in Clarkesworld and FIYAH, and he was a writer-in-residence at Beijing's Red Gate Gallery in 2019.</h4>
-                                    <h4>Kola has degrees in Computer Science and English from Amherst College, and he is currently represented by Matt Belford at the Tobias Literary Agency. You can find him on Twitter at <a href="https://twitter.com/KolaHR">@KolaHR</a>.</h4>
-                                </Text>
-                            </Layout>
-                        </TextBlock>
+                    <div class = "row justify-content-center align-items-center" style={{height: "80vh", margin: "20px"}}>
+                            <TextBlock>
+                                <Layout>
+                                    <Text>
+                                        <h4>Hey, thanks for visiting. My name's Kola, and I'm a speculative fiction writer and a junior scholar. I'm currently a Faculty Assistant at the University of Maryland's <a href="https://aadhum.umd.edu/" target="_blank" rel="noopener noreferrer">African-American History, Culture, and Digital Humanities initiative (AADHum)</a>. I'm from Durham, NC and I spent four years of my childhood/teens in Ile-Ife, Nigeria.</h4>
+                                        <h4>At the moment, I'm researching how marginalized communities around the world implement emerging tech to change their social and physical (and digital) environments. My undergrad thesis specifically looked at how these communities used virtual spaces and technology in subversive ways. Moments of disruption never fail to interest me.</h4>
+                                        <h4>My fiction is inspired by the same cultural and technological intersections--the new ways that the material world changes how people engage with each other. Though these narratives tend to manifest through prose, I've worked on some <a href="https://www.dropbox.com/sh/bgqnrvibjpxpcke/AACLSn9AVCGj3v5ew_IJsQg2a?dl=0" target="_blank" rel="noopener noreferrer">mixed media projects implementing video, glitch art, and fiction</a>. If you're looking for some straight prose, you can find my short stories in Clarkesworld and FIYAH.</h4>
+                                        <h4>I have degrees in Computer Science and English from Amherst College, and I'm currently represented by <a href="https://www.mattbelford.com/" target="_blank" rel="noopener noreferrer">Matt Belford</a> at <a href="https://www.thetobiasagency.com/">the Tobias Literary Agency</a>. Check out my Twitter at <a href="https://twitter.com/KolaHR" target="_blank" rel="noopener noreferrer">@KolaHR</a>. Also, here's a <a href="https://raw.githubusercontent.com/MonaLon/sitestuff/main/src/assets/headshot.jpg" target="_blank" rel="noopener noreferrer">headshot</a>.</h4>
+                                    </Text>
+                                </Layout>
+                            </TextBlock>
                     </div>
                 </div>
             </Theme>
-            </div>
         </Background>
     );
 };
