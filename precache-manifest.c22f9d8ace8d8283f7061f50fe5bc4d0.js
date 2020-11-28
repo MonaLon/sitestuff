00e31c547252ef32d30964eb6e46ced40c239feb
Kola Heyward-Rotimi <ttraveller7@gmail.com>
2020-11-27 20:29:03 -0500

    Updates

---
+++ precache-manifest.c22f9d8ace8d8283f7061f50fe5bc4d0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2129d3c1c16d3dd8d6411202c6506fb4",
+    "url": "/sitestuff/index.html"
+  },
+  {
+    "revision": "2068b50c90aeba220ca3",
+    "url": "/sitestuff/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "93ea6749aab5fae8d20f",
+    "url": "/sitestuff/static/js/2.41299ec5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/sitestuff/static/js/2.41299ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2068b50c90aeba220ca3",
+    "url": "/sitestuff/static/js/main.78fc9717.chunk.js"
+  },
+  {
+    "revision": "839af19973419393d6e4",
+    "url": "/sitestuff/static/js/runtime-main.e5a90599.js"
+  },
+  {
+    "revision": "ea675ae0ed8cb9bca651c4eff3fb7625",
+    "url": "/sitestuff/static/media/backgroundAbout.ea675ae0.jpg"
+  },
+  {
+    "revision": "b6aeb2047cbdd43f792339aafc84909f",
+    "url": "/sitestuff/static/media/backgroundCV.b6aeb204.jpg"
+  },
+  {
+    "revision": "ad3b1204f2cd07ba73ede5cbfd988bea",
+    "url": "/sitestuff/static/media/backgroundContact.ad3b1204.jpg"
+  },
+  {
+    "revision": "f0a04d7f36639e77a34573011c9996ec",
+    "url": "/sitestuff/static/media/backgroundHome.f0a04d7f.jpg"
+  },
+  {
+    "revision": "86db2c71b7fdd48b708bdd466d5ec069",
+    "url": "/sitestuff/static/media/backgroundWork.86db2c71.jpg"
+  },
+  {
+    "revision": "3fe22b82aec9f298703069d827921d54",
+    "url": "/sitestuff/static/media/headshot.3fe22b82.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sitestuff/static/media/logo.5d5d9eef.svg"
+  }
+]);
