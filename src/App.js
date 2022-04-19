c124fdd40d6db872a6cb504e2c97ac0470fe118c
Kola Heyward-Rotimi <ttraveller7@gmail.com>
2022-04-19 19:54:24 +0300

    trimming
    gradual process, but will need to take out a lot of superfluous pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
 import logo from './logo.svg';
 import './App.css';
-import Theme from './Theme';
 import styled from 'styled-components';
 
 const Text = styled.div`
@@ -23,6 +22,7 @@ const Text = styled.div`
     font-size: 64pt;
     letter-spacing: -2px;
     line-height: 90px;
+    margin-bottom: 30px;
 
     a {
         color: black;
@@ -35,20 +35,21 @@ class App extends Component {
     return (
       <React.Fragment>
         <div class = "container-fluid" style={{height: "100vh"}}>
-            <Text><p>I'm Kola. I'm a PhD student at <a href = "https://mtl.stanford.edu" target="_blank">Stanford</a>. I was a Faculty Research Assistant at the <a href="https://mith.umd.edu/" target="_blank">University of Maryland, College Park</a>.</p>
+            <Text><p>I'm Kola. I'm a Faculty Research Assistant at the <a href="https://mith.umd.edu/" target="_blank">University of Maryland, College Park</a>. I'll be a PhD student at <a href = "https://mtl.stanford.edu" target="_blank">Stanford</a> starting this Fall. Part of my work is speculative.</p>
             <p>⬤</p>
             <p>Here are some things I've written:</p>
-            <p><a href = "https://logicmag.io/clouds/" target="_blank">The Barn</a></p>
-            <p><a href = "https://reckoning.press/reckoning-6/" target="_blank">E.I.</a></p>
-            <p><a href = "https://bloodknife.com/sifting-through-the-future-present/" target="_blank">Sifting Through the Future-Present</a></p>
-            <p><a href = "https://two.compost.digital/qubit-guarantee/" target="_blank">Qubit Guarantee</a></p>
-            <p><a href = "http://strangehorizons.com/fiction/an-exploration-of-nichole-otienos-early-filmography-1232-1246/" target="_blank">An Exploration of Nichole Otieno's Early Filmography (1232-1246)</a></p>
-            <p><a href = "https://kola.legumenation.com/kola/index" target="_blank">Technological Disruptions: The Interplay of Subjective Flaws and Virtual Space</a></p>
-            <p><a href = "https://www.fiyahlitmag.com/issues/issue-9/" target="_blank" rel="noopener noreferrer">Gloss</a></p>
-            <p><a href = "https://clarkesworldmagazine.com/heyward-rotimi_10_15/" target="_blank">The Father</a></p>
+            <p>•<a href = "https://reviewsindh.pubpub.org/pub/black-meme/release/1" target = "_blank">Review: BLACK MEME</a></p>
+            <p>•<a href = "https://logicmag.io/clouds/" target="_blank">The Barn</a></p>
+            <p>•<a href = "https://reckoning.press/reckoning-6/" target="_blank">E.I.</a></p>
+            <p>•<a href = "https://bloodknife.com/sifting-through-the-future-present/" target="_blank">Sifting Through the Future-Present</a></p>
+            <p>•<a href = "https://two.compost.digital/qubit-guarantee/" target="_blank">Qubit Guarantee</a></p>
+            <p>•<a href = "http://strangehorizons.com/fiction/an-exploration-of-nichole-otienos-early-filmography-1232-1246/" target="_blank">An Exploration of Nichole Otieno's Early Filmography (1232-1246)</a></p>
+            <p>•<a href = "https://kola.legumenation.com/kola/index" target="_blank">Technological Disruptions: The Interplay of Subjective Flaws and Virtual Space</a></p>
+            <p>•<a href = "https://www.fiyahlitmag.com/issues/issue-9/" target="_blank" rel="noopener noreferrer">Gloss</a></p>
+            <p>•<a href = "https://clarkesworldmagazine.com/heyward-rotimi_10_15/" target="_blank">The Father</a></p>
             <p>⬤</p>
             <p>Here's my <a href = "https://drive.google.com/file/d/1yvXytyBsTRYHSQjrS-0s-oUjgHSFdyNo/view?usp=sharing" target="_blank">CV</a> and <a href = "https://twitter.com/KolaHR" target="_blank">Twitter</a>.</p>
-            <p>kolaheywardrotimi at gmail.com</p></Text>
+            <p style = {{ fontSize: '40pt' }}>kolaheywardrotimi at gmail.com</p></Text>
           </div>
       </React.Fragment>
     );
